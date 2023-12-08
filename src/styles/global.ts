802f965d3c4eb34b1c6d264cbafcb369955d4532
erikarosah <erikarosamg@gmail.com>
2023-12-08 16:46:52 -0300

    initial styling

---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #0C1317;
+        --shape: #202C33;
+        --chat: #111B21;
+        --text-title: #E4EBEB;
+        --text-span: #819096
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1000px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 300;
+        color: var(--text-title);
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 400;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
